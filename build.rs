4e96f5612f4f989132ebbec0149f6abfe205907f
docfate111 <tdwilliamsiv@gmail.com>
2022-09-11 18:23:32 -0700

    booting kernel in rust userspace

---
+++ build.rs
@@ -0,0 +1,9 @@
+use std::env;
+
+fn main() {
+    let project_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    println!("cargo:rustc-link-search={}", project_dir); // the "-L" flag
+    println!("cargo:rustc-link-lib=lkl"); // the "-l" flag
+}
+

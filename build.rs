5761c951579d81fde8db4e76ae31acf3a0a00d54
docfate111 <tdwilliamsiv@gmail.com>
2022-09-17 21:46:03 -0700

    new build script test who dis

--- build.rs
+++ build.rs
@@ -1,9 +1,26 @@
 use std::env;
+use std::path::Path;
+use std::process::Command;
 
 fn main() {
     let project_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-
+    if !Path::new("./liblkl.so").exists() {
+        Command::new("git")
+            .args(&["clone", "https://github.com/lkl/linux.git"])
+            .current_dir(&Path::new(&project_dir))
+            .status()
+            .unwrap();
+        Command::new("make")
+            .args(&["ARCH=lkl", "-C", "linux/tools/lkl"])
+            .current_dir(&Path::new(&project_dir))
+            .status()
+            .unwrap();
+        Command::new("cp")
+            .args(&["linux/tools/lkl/lib/liblkl.so", "."])
+            .current_dir(&Path::new(&project_dir))
+            .status()
+            .unwrap();
+    }
     println!("cargo:rustc-link-search={}", project_dir); // the "-L" flag
     println!("cargo:rustc-link-lib=lkl"); // the "-l" flag
 }
-

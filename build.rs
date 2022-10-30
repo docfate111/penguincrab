0b83e9b76111df2b52ab867208cec2c714af09f1
docfate111 <tdwilliamsiv@gmail.com>
2022-10-29 20:56:44 -0700

    update to linux 5.18

--- build.rs
+++ build.rs
@@ -6,7 +6,7 @@ fn main() {
     let project_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     if !Path::new("./liblkl.so").exists() {
         Command::new("git")
-            .args(&["clone", "https://github.com/lkl/linux.git"])
+            .args(&["clone", "-b", "merge-5.18", "https://github.com/thehajime/linux.git"])
             .current_dir(&Path::new(&project_dir))
             .status()
             .unwrap();
